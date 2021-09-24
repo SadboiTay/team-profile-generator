d5bcec724dbd1ab027adfb4db29e527df779fdd0
SadboiTay <tg.hakes@gmail.com>
2021-09-24 15:51:40 -0600

    create manager constructor- passes tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,42 @@
+const Manager = require('../lib/Manager');
+
+const billMockData = {
+    name: 'Bill',
+    id: 2,
+    email: "billsworld@yahoo.com",
+    officeNumber: 123
+}
+
+test('creates manager object', () => {
+    const manager = new Manager(billMockData);
+
+    expect(manager).toEqual(expect.any(Object));
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+})
+
+test('gets name value', () => {
+    const manager = new Manager(billMockData);
+
+    expect(manager.getName()).toEqual('Bill');
+})
+
+test('gets id value', () => {
+    const manager = new Manager(billMockData);
+
+    expect(manager.getId()).toEqual(manager.id);
+})
+
+test('gets email value', () => {
+    const manager = new Manager(billMockData);
+
+    expect(manager.getEmail()).toEqual(manager.email);
+})
+
+test('gets role value, overriden from employee', () => {
+    const manager = new Manager(billMockData);
+
+    expect(manager.getRole()).toEqual("Manager");
+})
